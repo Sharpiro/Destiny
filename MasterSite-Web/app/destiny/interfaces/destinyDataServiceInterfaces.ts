8c8a261b122848fb931566c6e51af9716d18079e
sharpiro <dsharpbb09@gmail.com>
2015-07-31 17:33:37 -0400

    Added icons. Set icons to get securely from bungie.net instead of proxy to allow for browser caching Added Light level to gear. Added Damage Type to Weapons refactored logic to allow ordering of inventory items and generic item data to be completely separate

---
+++ MasterSite-Web/app/destiny/interfaces/destinyDataServiceInterfaces.ts
@@ -0,0 +1,25 @@
+﻿interface IDestinyDataService
+{
+    getClassHashes(): Array<IHash>;
+}
+
+interface IHash
+{
+    hash: number;
+    value: string;
+}
+
+interface IBucketHash extends IHash {
+    category: number;
+}
+
+interface IStaticTriumphsData
+{
+    yearOne: Array<IYearOneTriumph>;
+}
+
+interface IYearOneTriumph
+{
+    title: string;
+    description: string;
+}
